7f23251b488f11b04f84fd869554ae24f2f2bd34
saranya <saranyamahe77@gamil.com>
2021-02-07 23:24:09 +0530

    first commit

---
+++ src/sub/java/sub.java
@@ -0,0 +1,25 @@
+package sub.java;
+
+import java.util.Scanner;
+
+public class sub {
+	public static void main(String [] args) 
+	{
+	int a,b;
+	Scanner sc	=new Scanner(System.in);	
+	System.out.println("enter first number ");
+
+	a=sc.nextInt();
+	System.out.println("enter second numbe");
+	b=sc.nextInt();
+	int c =a-b;
+	
+	
+	System.out.println("total :"+c);
+	
+			
+	
+	
+	
+	}
+}
